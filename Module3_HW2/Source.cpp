454d1d2136cbf3c8b3c34e438e2e1265e71fb3ab
dinara0708 <aikenova01@gmail.com>
2018-06-10 13:12:39 +0600

    -

---
+++ Module3_HW2/Source.cpp
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<math.h>
+#include<time.h>
+#include <Windows.h>
+
+void main()
+{
+
+	setlocale(LC_ALL, "Rus");
+
+	srand(time(NULL));
+
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	int tn = 0;
+
+start:
+
+	printf("task number:\t");
+	scanf("%d", &tn);
+
+	switch (tn)
+	{
+	case 1:
+	{
+
+	}
+	break;
+	case 2:
+	{
+
+	}
+	break;
+	case 3:
+	{
+
+	}
+	break;
+	case 4:
+	{
+
+	}
+	break;
+	case 5:
+	{
+
+	}
+	break;
+	case 6:
+	{
+
+	}
+	break;
+	case 7:
+	{
+
+	}
+	break;
+	default:
+		printf("\nerror: no tasks\n\n");
+	}
+	goto start;
+}
